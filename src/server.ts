6388992ebf4b1b9bfea4101c8514161a409f0a5d
Gene Hall <genehall@eworldes.com>
2023-05-15 09:51:39 -1000

    middleware

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
-import express from 'express';
-import router from './router'
+import express from "express";
+import router from "./router";
+import morgan from "morgan";
 
 const app = express();
 
-app.get('/', (req, res) => {
-    console.log('hellor from express');
-    res.status(200);
-    res.json({ message: 'yo' });
-})
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', router);
+app.get("/", (req, res) => {
+  console.log("hellor from express");
+  res.status(200);
+  res.json({ message: "yo" });
+});
+
+app.use("/api", router);
 
 export default app;
