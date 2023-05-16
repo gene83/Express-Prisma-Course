39534a5a4f6ae9174256de1c5f90793491ef7e77
Gene Hall <genehall@eworldes.com>
2023-05-16 12:10:03 -1000

    example user handler test

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,28 @@
+import * as user from '../user';
+
+/**
+ * use before all or config to clear database after each suite
+ * 
+ */
+
 describe('user handler', () => {
-    it('should do something when something happens', () => {
-        expect(1).toBe(1);
+    it('Should create a new user', async () => {
+        const req = {
+            body: {
+                username: 'test',
+                password: 'password',
+            }
+        };
+
+        const res = {
+            json({token}) {
+                console.log(token)
+                expect(token).toBeTruthy();
+            }
+        }
+
+        await user.createNewUser(req, res, () => {});
+
+        // expect to see at least one expect call
     })
 })
